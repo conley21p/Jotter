0df9f6438d41031c4d9502533380135d6d1bee34
Noah Conn <86074627+noah-conn@users.noreply.github.com>
2022-03-15 21:38:34 -0500

    created register page and servlet

--- src/main/java/Account/Account.java
+++ src/main/java/Account/Account.java
@@ -1,22 +1,18 @@
 package Account;
 
 public class Account {
+    private String email;
     private String username;
     private String password;
-    private String firstName;
-    private String lastName;
-    private String email;
 
-    public Account(String user, String pass, String fName, String lName, String email) {
+    public Account(String user, String pass, String email) {
         this.username = user;
         this.password = pass;
-        this.firstName = fName;
-        this.lastName = lName;
         this.email = email;
     }
 
     public String exportAsCsvString() {
-        return username + "," + password + "," + firstName + "," + lastName + "," + email;
+        return email + "," + username + "," + password;
     }
 
     public String getUsername() {
@@ -29,22 +25,6 @@ public class Account {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String fName) {
-        this.firstName = fName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setlLastName(String lName) {
-        this.lastName = lName;
     }
 
     public String getEmail() {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -11,6 +11,7 @@
 <h1><%= " Today's TO-Do list" %>
 </h1>
 <br/>
+<a href="RegisterServlet">Register</a>
 <a href="AddAssignmentServlet">Click To Add an Assignment</a>
 
 <h1>Assignments</h1>
