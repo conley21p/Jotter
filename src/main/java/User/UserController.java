2a1f51874bc883f49842c52400f1a83e005a7b92
jradtke65 <99921901+jradtke65@users.noreply.github.com>
2022-04-26 18:10:28 -0500

    Adding UserController

---
+++ src/main/java/User/UserController.java
@@ -0,0 +1,50 @@
+package User;
+
+import calendar.Calendar;
+
+public class UserController {
+    private static String username;
+    private static String password;
+    private static String[] calendarNames;
+    private static Calendar curCalendar;
+
+    public static void loadUser(String user, String pass, String[] calNames, Calendar curCal)
+    {
+        username = user;
+        password = pass;
+        calendarNames = calNames;
+        curCalendar = curCal;
+    }
+
+    public static void dropUser() {
+        username = null;
+        calendarNames = null;
+        curCalendar = null;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String user) { username = user; }
+
+    public static String getPassword() { return password; }
+
+    public static void setPassword(String pass) { password = pass; }
+
+    public static String[] getCalendarNames() {
+        return calendarNames;
+    }
+
+    public static void setCalendarNames(String[] calNames) {
+        calendarNames = calNames;
+    }
+
+    public static Calendar getCurCalendar() {
+        return curCalendar;
+    }
+
+    public static void setCurCalendar(Calendar calendar) {
+        curCalendar = calendar;
+    }
+}
