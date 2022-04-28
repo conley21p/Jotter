2d18ab13acfb95ebec60bdaba3f4437e9e3f358c
jradtke65 <99921901+jradtke65@users.noreply.github.com>
2022-04-28 14:14:00 -0500

    Adding Course Attribute to Assignment

--- src/main/java/servlets/ImportCalendarServlet.java
+++ src/main/java/servlets/ImportCalendarServlet.java
@@ -6,8 +6,6 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 
 
-import java.io.*;
-
 @WebServlet(name = "ImportCalendarServlet", value = "/ImportCalendarServlet")
 @MultipartConfig(
         fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
@@ -19,23 +17,9 @@ public class ImportCalendarServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("forward to file.jsp");
-        response.setContentType("image/jpeg");
-        ServletOutputStream out;
-        out = response.getOutputStream();
-        FileInputStream fin = new FileInputStream("c:\\test\\java.jpg");
-
-        BufferedInputStream bin = new BufferedInputStream(fin);
-        BufferedOutputStream bout = new BufferedOutputStream(out);
-        int ch =0; ;
-        while((ch=bin.read())!=-1)
-        {
-            bout.write(ch);
-        }
-
-        bin.close();
-        fin.close();
-        bout.close();
-        out.close();
+        /*
+            Send user to the add assignment page
+         */
         getServletContext().getRequestDispatcher("/file.jsp").forward(request,response);
     }
 
