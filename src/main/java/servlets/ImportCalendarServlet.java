122a0ad01f959e0fb436405b16757f0263d0e8b9
jradtke65 <99921901+jradtke65@users.noreply.github.com>
2022-04-30 14:58:53 -0500

    Test of Import Cal

--- src/main/java/servlets/ImportCalendarServlet.java
+++ src/main/java/servlets/ImportCalendarServlet.java
@@ -28,7 +28,7 @@ public class ImportCalendarServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String uploadPath = PathFinder.getAccountCalendarsPath(HomePageServlet.user.getUsername());
         File uploadDir = new File(uploadPath);
-        if (!uploadDir.exists()) uploadDir.mkdir();
+        //if (!uploadDir.exists()) uploadDir.mkdir();
 
         for (Part part : request.getParts()) {
             String fileName = part.getSubmittedFileName();
