6dd6241ce6eeab71725cf92923ad59ef7f34edfb
jradtke65 <99921901+jradtke65@users.noreply.github.com>
2022-04-30 14:08:15 -0500

    Testing

--- src/main/java/servlets/ImportCalendarServlet.java
+++ src/main/java/servlets/ImportCalendarServlet.java
@@ -1,17 +1,17 @@
 package servlets;
+import utils.PathFinder;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.io.File;
 import java.io.IOException;
 
 
 @WebServlet(name = "ImportCalendarServlet", value = "/ImportCalendarServlet")
-@MultipartConfig(
-        fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
-        maxFileSize = 1024 * 1024 * 10,      // 10 MB
-        maxRequestSize = 1024 * 1024 * 100   // 100 MB
-)
+@MultipartConfig(fileSizeThreshold = 1024 * 1024,
+        maxFileSize = 1024 * 1024 * 5,
+        maxRequestSize = 1024 * 1024 * 5 * 5)
 
 public class ImportCalendarServlet extends HttpServlet {
     @Override
@@ -26,5 +26,6 @@ public class ImportCalendarServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.sendRedirect("HomePageServlet");
+        File accountInfoFile = new File(PathFinder.getAccountInformationPath(HomePageServlet.user.getUsername()));
     }
 }
