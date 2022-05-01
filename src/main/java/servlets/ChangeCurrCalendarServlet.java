31e4c368a451360ea9a3d3f4a89fbdb6b63c0892
jradtke65 <99921901+jradtke65@users.noreply.github.com>
2022-05-01 18:43:56 -0500

    Creating Analytics

--- src/main/java/servlets/ChangeCurrCalendarServlet.java
+++ src/main/java/servlets/ChangeCurrCalendarServlet.java
@@ -7,13 +7,14 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet(name = "ChangeCurrCalendarServlet", urlPatterns = "/ChangeCurrCalendar")
 public class ChangeCurrCalendarServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("calendars", HomePageServlet.user.getCalendarNames());
-        //request.setAttribute("size",        HomePageServlet.user.getCurrCal().getCalendarObjList().size());
+        request.setAttribute("size", HomePageServlet.user.getCalendarNames().length);
         //request.setAttribute("calName",     HomePageServlet.user.getCurrCal().getName());
 
         /*
@@ -26,7 +27,9 @@ public class ChangeCurrCalendarServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //Need to figure out how to get the correct calendar name.
-        HomePageServlet.user.setCurrCal(CalendarController.getCalendar(HomePageServlet.user.getUsername(), ""));
+        Enumeration<String> parameterNames = request.getParameterNames();
+        String assignName = parameterNames.nextElement();
+        HomePageServlet.user.setCurrCal(CalendarController.getCalendar(HomePageServlet.user.getUsername(), assignName));
         response.sendRedirect("/HomePageServlet");
     }
 }
