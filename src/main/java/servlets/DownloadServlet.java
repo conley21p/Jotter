ef8f5cde94d3f01c260f4c18a7015e9662c2704c
jradtke65 <99921901+jradtke65@users.noreply.github.com>
2022-04-30 23:05:53 -0500

    Export Current Calendar Test

--- src/main/java/servlets/DownloadServlet.java
+++ src/main/java/servlets/DownloadServlet.java
@@ -4,15 +4,6 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @WebServlet(name = "DownloadServlet", value = "/DownloadServlet")
 public class DownloadServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -23,7 +14,8 @@ public class DownloadServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         String fileName = HomePageServlet.user.getCurrCal().getName();
         String filePath = utils.PathFinder.getAccountCalendarsPath(HomePageServlet.user.getUsername());
-        response.setContentType("APPLICATION/OCTET-STREAM");
+
+        response.setContentType("text/plain");
         response.setHeader("Content-Disposition", "attachment; filename=\""
                 + fileName + "\"");
 
