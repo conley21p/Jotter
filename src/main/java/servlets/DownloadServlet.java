c2b310e0b6a6565aad99189e17fda810af02bcb3
jradtke65 <99921901+jradtke65@users.noreply.github.com>
2022-04-30 23:12:10 -0500

    Export Current Calendar Test

--- src/main/java/servlets/DownloadServlet.java
+++ src/main/java/servlets/DownloadServlet.java
@@ -10,6 +10,7 @@ public class DownloadServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        System.out.println("Test");
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
