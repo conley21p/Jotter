fd1766bcc90e9089e639667749c27268a4a5d1cc
jradtke65 <99921901+jradtke65@users.noreply.github.com>
2022-04-30 20:45:11 -0500

    Test of Change Cal

--- src/main/java/servlets/ChangeCurrCalendarServlet.java
+++ src/main/java/servlets/ChangeCurrCalendarServlet.java
@@ -25,7 +25,7 @@ public class ChangeCurrCalendarServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.sendRedirect("/HomePageServlet");
     }
 }
 
