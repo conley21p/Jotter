026773db2a9da9b62ae674de677cf16e43a0b009
jradtke65 <99921901+jradtke65@users.noreply.github.com>
2022-04-30 22:56:02 -0500

    Export Current Calendar Test

--- src/main/java/servlets/DownloadServlet.java
+++ src/main/java/servlets/DownloadServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "DownloadServlet", value = "/DownloadServlet")
 public class DownloadServlet extends HttpServlet {
-
+    private static final long serialVersionUID = 1L;
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
