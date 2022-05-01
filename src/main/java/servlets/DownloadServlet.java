50d748878df41d41d91b5363a76a19559e5a9b3a
jradtke65 <99921901+jradtke65@users.noreply.github.com>
2022-04-30 23:25:56 -0500

    Export Current Calendar Test

--- src/main/java/servlets/DownloadServlet.java
+++ src/main/java/servlets/DownloadServlet.java
@@ -9,14 +9,17 @@ public class DownloadServlet extends HttpServlet {
     private final int ARBITARY_SIZE = 1048;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        resp.setContentType("text/plain");
-        resp.setHeader("Content-disposition", "attachment; filename=sample.txt");
+        response.setContentType("text/plain");
+        response.setHeader("Content-disposition", "attachment; filename=sample.txt");
+        String filePath = utils.PathFinder.getAccountCalendarsPath(HomePageServlet.user.getUsername());
+        String fileName = HomePageServlet.user.getCurrCal().getName();
+        String file = filePath + "/" + fileName;
 
-        try(InputStream in = req.getServletContext().getResourceAsStream("/WEB-INF/sample.txt");
-            OutputStream out = resp.getOutputStream()) {
+        try(InputStream in = request.getServletContext().getResourceAsStream(file);
+            OutputStream out = response.getOutputStream()) {
 
             byte[] buffer = new byte[ARBITARY_SIZE];
 
