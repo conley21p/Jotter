c33d17738ba4d8c353020efde1dac5507e261aa3
conley21p <conley21p@gmial.com>
2022-05-02 23:56:33 -0500

    fixed junit test

--- src/test/java/JunitTests/AssignmentTest.java
+++ src/test/java/JunitTests/AssignmentTest.java
@@ -36,28 +36,47 @@ class AssignmentTest {
                 "327",
                 "2022/05/01 18:12");
         System.out.println("View ToString:" + assign.toString());
+        assertEquals(true,test1);
+        assertEquals("2022-06-13,8:55,A,Final Exam,Comprehensive final for 327,327,2022/05/01 18:12,null",
+                assign.toString());
+
         boolean test2 = assign.edit("Final Exam for 327",
                 new Date("2022-6-13"),
                 new Time("08:55"),
                 "Comprehensive final for 327",
                 "356",
-                "2022/05/01 18:12");
+                "null");
         System.out.println("View ToString:" + assign.toString());
+        assertEquals(true,test2);
+        assertEquals("2022-06-13,8:55,A,Final Exam for 327,Comprehensive final for 327,356,null,null",
+                assign.toString());
+
         boolean test3 = assign.edit("Final Exam, for, 327",
                 new Date("2022-6-13"),
                 new Time("00:55"),
                 "Comprehensive, final, for 327",
                 "356",
-                "2022/05/01 18:12");
+                "null");
         System.out.println("View ToString:" + assign.toString());
+        assertEquals(true,test3);
+        assertEquals("2022-06-13,0:55,A,Final Exam for 327,Comprehensive final for 327,356,null,null",
+                assign.toString());
+
         boolean test4 = assign.edit("Final Exam for 356",
                 new Date("2022-5-13"),
                 new Time("00:55"),
                 "Comprehensive final for 356",
                 "356",
-                "2022/05/01 18:12");
+                "2022/04/01 18:12");
         System.out.println("View ToString:" + assign.toString());
-        System.out.println("Test results 1:"+ test1 + " 2:" + test2 + " 3:" + test3 + " 4:" + test4);
+        assertEquals(true,test4);
+        assertEquals("2022-05-13,0:55,A,Final Exam for 356,Comprehensive final for 356,356,2022/04/01 18:12,null",
+                assign.toString());
+
+
+        if (test1 || test2 || test3 || test4){
+            System.out.println("Test results 1:"+ test1 + " 2:" + test2 + " 3:" + test3 + " 4:" + test4);
+        }
     }
 
 
--- src/test/java/JunitTests/CalendarControllerTest.java
+++ src/test/java/JunitTests/CalendarControllerTest.java
@@ -1,19 +1,24 @@
-package calendar;
+package JunitTests;
 
+import calendar.Assignment;
+import calendar.Time;
+import calendar.Date;
+import calendar.CalendarController;
+import servlets.HomePageServlet;
 import User.User;
 import account.AccountManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import servlets.HomePageServlet;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalendarControllerTest {
 
     String userName = "deleteTest";
     String password = "password";
     String email    = "test@gmail.com";
+    Assignment[] testAssigns = new Assignment[4];
 
     @BeforeEach
     void setUp() {
@@ -26,38 +31,42 @@ class CalendarControllerTest {
                 calendarNameList,
                 CalendarController.getCalendar(userName,
                         "School"));
+        testAssigns[0] = new Assignment( "Final Exam",
+                                        new Date("2022-6-13"),
+                                        new Time("08:55"),
+                                        "Comprehensive final for 327",
+                                        "327",
+                                        "null",
+                                        "null");
+        testAssigns[1] = new Assignment( "Final Exam 356",
+                                        new Date("2022-7-13"),
+                                        new Time("14:55"),
+                                        "Comprehensive final for 356",
+                                        "356",
+                                        "null",
+                                        "null");
+        testAssigns[2] = new Assignment( "Final HW",
+                                        new Date("2022-6-13"),
+                                        new Time("23:55"),
+                                        "Cost models homwork",
+                                        "327",
+                                        "null",
+                                        "null");
+        testAssigns[3] = new Assignment( "Final Exam",
+                                        new Date("2022-8-13"),
+                                        new Time("08:55"),
+                                        "Comprehensive final for 326",
+                                        "326",
+                                        "null",
+                                        "null");
         HomePageServlet.user.getCurrCal().addNewToCalendarObjList(userName,
-                new Assignment( "Final Exam",
-                        new Date("2022-6-13"),
-                        new Time("08:55"),
-                        "Comprehensive final for 327",
-                        "327",
-                        "null",
-                        "null"));
+                                                                    testAssigns[0]);
         HomePageServlet.user.getCurrCal().addNewToCalendarObjList(userName,
-                new Assignment( "Final Exam 356",
-                        new Date("2022-7-13"),
-                        new Time("14:55"),
-                        "Comprehensive final for 356",
-                        "356",
-                        "null",
-                        "null"));
+                                                                    testAssigns[1]);
         HomePageServlet.user.getCurrCal().addNewToCalendarObjList(userName,
-                new Assignment( "Final HW",
-                        new Date("2022-6-13"),
-                        new Time("23:55"),
-                        "Cost models homwork",
-                        "327",
-                        "null",
-                        "null"));
+                                                                    testAssigns[2]);
         HomePageServlet.user.getCurrCal().addNewToCalendarObjList(userName,
-                new Assignment( "Final Exam",
-                        new Date("2022-8-13"),
-                        new Time("08:55"),
-                        "Comprehensive final for 326",
-                        "326",
-                        "null",
-                        "null"));
+                                                                    testAssigns[3]);
     }
 
     @AfterEach
@@ -71,14 +80,10 @@ class CalendarControllerTest {
         Assignment testResult2 = (Assignment) CalendarController.deleteCalendarObject(1);
         Assignment testResult3 = (Assignment) CalendarController.deleteCalendarObject(0);
         Assignment testResult4 = (Assignment) CalendarController.deleteCalendarObject(0);
-        if( testResult1 == null
-                ||  testResult2 == null
-                ||  testResult3 == null
-                ||  testResult4 == null){
-            System.out.println("test results 1:" +testResult1 +
-                    " 2:" + testResult2 +
-                    " 3:" + testResult3 +
-                    " 4:" + testResult4);
-        }
+
+        assertEquals(testAssigns[3],testResult1);
+        assertEquals(testAssigns[1],testResult2);
+        assertEquals(testAssigns[0],testResult3);
+        assertEquals(testAssigns[2],testResult4);
     }
 }
